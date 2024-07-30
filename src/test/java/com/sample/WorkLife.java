3226825ba15afc284738b928dac68c87f29c7792
karkuzhalikumaresan <karkuzhalikumaresan@gmail.com>
2024-07-30 15:42:01 +0530

    initial project

---
+++ src/test/java/com/sample/WorkLife.java
@@ -0,0 +1,17 @@
+package com.sample;
+
+public class WorkLife {
+	
+	public void sum() {
+		int sum=0;
+		int a=10;
+		int b=20;
+		sum=a+b;
+		System.out.println(sum);
+	}
+	public static void main(String[] args) {
+		WorkLife w=new WorkLife();
+		w.sum();
+	}
+
+}
